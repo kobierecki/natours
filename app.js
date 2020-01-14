b4771b32c1936ffa0d6f73f9118adfe5bae8db69
Oskar Kobierecki <okobierecki@empik.com>
2020-01-14 18:22:28 +0100

    Basic API

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours
+        },
+    })
+});
+// app.get('/', (req, res) => {
+//     res
+//         .status(200)
+//         .json({ message: 'Hello', app: 'Natours' });
+// });
+
+// app.post('/', (req, res) => {
+//     res.send('You can post to this endpoint')
+// });
+
+
+const port = 3000
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+});
