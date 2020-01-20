50624af67d2b1903e06f0ad1075e060fd6464ac7
Oskar Kobierecki <okobierecki@empik.com>
2020-01-20 15:10:02 +0100

    Finished API, added routers and handlers

--- app.js
+++ app.js
@@ -1,33 +1,23 @@
 const express = require('express');
-const fs = require('fs');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
+// MIDDLEWARES 
 
-app.get('/api/v1/tours', (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        results: tours.length,
-        data: {
-            tours
-        },
-    })
-});
-// app.get('/', (req, res) => {
-//     res
-//         .status(200)
-//         .json({ message: 'Hello', app: 'Natours' });
-// });
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 
-// app.post('/', (req, res) => {
-//     res.send('You can post to this endpoint')
-// });
+app.use(express.json());
+app.use(express.static(`${__dirname}/public/`));
 
+// ROUTES
 
-const port = 3000
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-});
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app;
