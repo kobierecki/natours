aa3f2f7473e1b0bc40e09b1dfe50b28b73807895
Oskar Kobierecki <okobierecki@empik.com>
2020-01-30 15:00:47 +0100

    Finished error controller

--- controllers/errorControllers.js
+++ controllers/errorControllers.js
@@ -1,3 +1,21 @@
+const AppError = require('../utils/appError');
+
+const handleValidationErrorDB = err => {
+	const errors = Object.values(err.errors).map(el => el.message);
+	const message = `Invalid inpyt data: ${errors.join('. ')}`;
+	return new AppError(message, 400);
+}
+
+const handleDuplicateFieldsDB = err => {
+	const message = `Duplicate field value: ${err.keyValue.name}. Pleasse use another value!`;
+	return new AppError(message, 404);
+};
+
+const handleCastErrorDB = err => {
+	const message = `Invalid ${err.path}: ${err.value}`;
+	return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
 	res.status(err.statusCode).json({
 		error: err,
@@ -14,7 +32,6 @@ const sendErrorProd = (err, res) => {
 			message: err.message
 		});
 	} else {
-		console.error('Error', err);
 		res.status(500).json({
 			status: 'error',
 			message: 'Something went wrong'
@@ -23,13 +40,16 @@ const sendErrorProd = (err, res) => {
 };
 
 module.exports = (err, req, res, next) => {
-	console.log(err.stack);
 	err.statusCode = err.statusCode || 500;
 	err.status = err.status || 'error';
 
 	if (process.env.NODE_ENV === 'development') {
 		sendErrorDev(err, res);
 	} else if (process.env.NODE_ENV === 'production') {
-		sendErrorProd(err, res);
+		let error = { ...err };
+		if (error.name === 'CastError') error = handleCastErrorDB(error);
+		if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+		if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
+		sendErrorProd(error, res);
 	}
 };
--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose');
-
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+
+process.on('uncaughtException', err => {
+	console.log(err.name, err.message);
+	console.log('UNCAUGHT EXCEPTION!');
+	server.close(() => {
+		process.exit(1);
+	});
+});
+
 const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
@@ -21,6 +29,14 @@ mongoose
 	});
 
 const port = process.env.PORT || 8000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
 	console.log(`Server is running on port ${port}`);
+});
+
+process.on('unhandledRejection', err => {
+	console.log(err.name, err.message);
+	console.log('UNHANDLED REJECTION!');
+	server.close(() => {
+		process.exit(1);
+	});
 });
